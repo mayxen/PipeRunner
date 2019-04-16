0c3bccbd79393b1ec9ecafe67b40486523fa94af
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-04-16 10:48:35 +0100

    mesh created

--- Assets/Proyecto/Scripts/Pipe.cs
+++ Assets/Proyecto/Scripts/Pipe.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +11,83 @@ public class Pipe : MonoBehaviour
     public int CurveSegmentCount;
     public int PipeSegmentCount;
 
-    private Vector3 GetPointsOnTorus(float u, float v)
+    public float RingDistance;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = mesh =  new Mesh();
+        mesh.name = "PipeMesh";
+        SetVertices();
+        SetTriangles();
+        mesh.RecalculateNormals();
+
+    }
+
+    private void SetTriangles()
+    {
+        triangles = new int[PipeSegmentCount * CurveSegmentCount * 6];
+        for (int t = 0,i=0; t < triangles.Length; t+=6,i+=4)
+        {
+            triangles[t] = i;
+            triangles[t + 1] = triangles[t + 4] = i + 1;
+            triangles[t + 2] = triangles[t + 3] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+        mesh.triangles = triangles;
+    }
+
+    private void SetVertices()
+    {
+        vertices = new Vector3[PipeSegmentCount * CurveSegmentCount * 4];
+        float uStep = RingDistance / CurveRadius;
+        CreateFirstQuadring(uStep);
+        int iDelta = PipeSegmentCount * 4;
+
+        for (int i = iDelta, u = 2; u <= CurveSegmentCount; u++, i += iDelta)
+        {
+            CreateQuadRing(u * uStep, i);
+        }
+
+        mesh.vertices = vertices;
+    }
+
+    private void CreateQuadRing(float u, int i)
+    {
+        float vStep = (2f * Mathf.PI) / PipeSegmentCount;
+        int ringOffSet = PipeSegmentCount * 4;
+
+        Vector3 vertex = GetPointOnTorus(u,0f);
+
+        for (int v = 1; v <= PipeSegmentCount; v++,i+=4)
+        {
+            vertices[i] = vertices[i - ringOffSet + 2];
+            vertices[i + 1] = vertices[i - ringOffSet + 3];
+            vertices[i + 2] = vertex;
+            vertices[i + 3] = vertex = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void CreateFirstQuadring(float uStep)
+    {
+        float vStep = (2f * Mathf.PI) / PipeSegmentCount;
+
+        Vector3 vertexA = GetPointOnTorus(0f, 0f);
+        Vector3 vertexB = GetPointOnTorus(uStep,0f);
+
+        for (int v = 1, i = 0; v <= PipeSegmentCount; v++,i += 4)
+        {
+            vertices[i] = vertexA;
+            vertices[i + 1] = vertexA =GetPointOnTorus(0f, v * vStep);
+            vertices[i + 2] = vertexB;
+            vertices[i + 3] = vertexB= GetPointOnTorus(uStep, v * vStep);
+        }
+    }
+
+    private Vector3 GetPointOnTorus(float u, float v)
     {
         Vector3 p;
         float r = (CurveRadius + PipeRadius * Mathf.Cos(v));
@@ -20,19 +97,19 @@ public class Pipe : MonoBehaviour
         return p;
     }
 
-    private void OnDrawGizmos()
-    {
-        float uStep = (2f * Mathf.PI) / CurveSegmentCount;
-        float vStep = (2f * Mathf.PI) / PipeSegmentCount;
+    //private void OnDrawGizmos()
+    //{
+    //    float uStep = (2f * Mathf.PI) / CurveSegmentCount;
+    //    float vStep = (2f * Mathf.PI) / PipeSegmentCount;
 
-        for (int u = 0; u < CurveSegmentCount; u++)
-        {
-            for (int v = 0; v < PipeSegmentCount; v++)
-            {
-                Vector3 point = GetPointsOnTorus(u * uStep, v * vStep);
-                Gizmos.color = new Color(1f, (float)v / PipeSegmentCount, (float)u / CurveSegmentCount);
-                Gizmos.DrawSphere(point,0.1f);
-            }
-        }
-    }
+    //    for (int u = 0; u < CurveSegmentCount; u++)
+    //    {
+    //        for (int v = 0; v < PipeSegmentCount; v++)
+    //        {
+    //            Vector3 point = GetPointOnTorus(u * uStep, v * vStep);
+    //            Gizmos.color = new Color(1f, (float)v / PipeSegmentCount, (float)u / CurveSegmentCount);
+    //            Gizmos.DrawSphere(point,0.1f);
+    //        }
+    //    }
+    //}
 }
